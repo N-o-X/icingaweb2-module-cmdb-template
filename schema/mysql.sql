d4adc2861ae770b59927cb1467cdcbb7f2abd497
Aigner,Martin <Martin.Aigner@drei.com>
2019-10-24 13:47:24 +0200

    added mysql ddl for config table

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -5,3 +5,29 @@ CREATE TABLE `host` (
 `created` datetime DEFAULT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE `config` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`type` varchar(64) NOT NULL,
+`hostname` varchar(64) NOT NULL,
+`servicename` varchar(64) NOT NULL,
+`monitorobject` varchar(64) NOT NULL,
+`msggroup` varchar(64) NOT NULL,
+`alertgroup` varchar(64) NOT NULL,
+`alertkey` varchar(64),
+`ciid` varchar(64) NOT NULL,
+`threshold_critical` int(10),
+`threshold_warning` int(10),
+`createdby` varchar(64) NOT NULL,
+`created` datetime DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+insert into config values (1,'filesystem','localhost','VirBox_filesystems','/etc','ENG-OSS','System','fs etc','vagrant-prod',98,20,'max muster',now());
+insert into config values (2,'filesystem','localhost','VirBox_filesystems','/var','ENG-OSS','System','fs var','vagrant-prod',98,20,'max muster',now());
+
+
+
+
+
