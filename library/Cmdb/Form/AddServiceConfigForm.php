8532b3fc48083e5e0f9d45512581078f252e4293
Noah Hilverling <noah@hilverling.com>
2019-10-24 15:57:31 +0200

    Add AddServiceConfigForm

---
+++ library/Cmdb/Form/AddServiceConfigForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Icinga\Module\Cmdb\Form;
+
+use Icinga\Module\Monitoring\Backend\MonitoringBackend;
+use ipl\Web\Compat\CompatForm;
+
+class AddServiceConfigForm extends CompatForm
+{
+    protected function getHostNames()
+    {
+        $hosts = MonitoringBackend::instance()->select()->from('hoststatus', [
+            'host_name'
+        ]);
+
+        return $hosts->fetchColumn();
+    }
+
+    protected function assemble()
+    {
+        $this->addElement('text', 'name', [
+            'label' => 'Service Name',
+            'placeholder' => 'Enter a service name',
+            'required' => true,
+        ]);
+
+        $this->addElement('select', 'host', array(
+            'label'        => 'Host',
+            'multiOptions' => $this->getHostNames(),
+            'required'     => true,
+        ));
+
+        $this->addElement('submit', 'submit', [
+            'label' => 'Add service',
+        ]);
+    }
+}
