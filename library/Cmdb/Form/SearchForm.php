57bf5d9efbd8e92f7616d6de318a17c98d28e716
Aigner,Martin <Martin.Aigner@drei.com>
2019-10-25 13:59:06 +0200

    search with autorefresh

--- library/Cmdb/Form/SearchForm.php
+++ library/Cmdb/Form/SearchForm.php
@@ -6,10 +6,14 @@ use ipl\Web\Compat\CompatForm;
 
 class SearchForm extends CompatForm
 {
+    protected $defaultAttributes = ['id' => 'cmdb-search-form' ];
+    
     protected function assemble()
     {
         $this->addElement('text', 'search', [
             'placeholder' => 'Search...',
+            //'class' => 'autosubmit'
+            'id' => 'cmdb-search'
         ]);
     }
 }
--- public/js/module.js
+++ public/js/module.js
@@ -12,13 +12,32 @@
             console.log('tst');
         },
         rendered: function(event) {
-            var $container = $('<div>');
-            console.dir(event);
+            
+            var $container = $('<div>');            
             $(event.target).append($container[0]);
             $('.cmdb-flatpickr').flatpickr({
                 appendTo: $container[0],
                 dateFormat: 'Y-m-d',
             });
+
+            var search = $('#cmdb-search');
+            
+            search.on('input',() => {
+                event.preventDefault();
+                console.log('event input received.....');
+                if (this.timeout !== undefined ) {
+                    clearTimeout(this.timeout);
+                }
+                this.timeout = setTimeout(this.submit,400);
+            });
+
+            var searchLength = search.val().length;
+            search.focus();
+            search[0].setSelectionRange(searchLength,searchLength);
+        },
+        submit: function() {
+            console.log('asdasd');
+            $('#cmdb-search-form').submit();
         }
     };
 
